b0b28ec8ac8ab622174c390800b1be06dccae93b
JohnCarlo <2213473@slu.edu.ph>
2026-07-30 02:50:15 +0800

    Added gmap location and major adjustments in About page

--- webapp/controller/About.controller.js
+++ webapp/controller/About.controller.js
@@ -1,8 +1,36 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
-  "use strict";
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+(
+    Controller
+) => {
 
-  return Controller.extend(
-    "com.john.employeeapp.com.john.employeeapp.controller.About",
-    {},
-  );
+    "use strict";
+
+
+    return Controller.extend(
+        "com.john.employeeapp.com.john.employeeapp.controller.About",
+        {
+
+
+            onOpenMap() {
+
+
+                const sMapUrl =
+                    this.getView()
+                        .getModel("about")
+                        .getProperty("/mapUrl");
+
+
+                window.open(
+                    sMapUrl,
+                    "_blank"
+                );
+
+
+            }
+
+
+        });
+
 });
